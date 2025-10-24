063f68caefe8d1bfdcbacf09ecfe4eaa9adce95b
Matheus Ruivo <matheushenriquerodriguesruivo4@gmail.com>
2025-10-24 02:33:11 -0300

    Add files via upload

---
+++ exer2.py
@@ -0,0 +1,8 @@
+#Segundo exercicio 05/10/25. acertei!
+nome = "Luiz Otávio"
+altura = 1.80
+peso = 95
+altura2 = 1.8 * 1.8
+conta = 95 / altura2 
+imc = conta
+print("Luiz Otávio tem",altura,"de altura, pesa", peso,"quilos e seu IMC é", imc) #correto!
